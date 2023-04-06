89ec985026dd2bd927da15949d201dc906d9a922
Jai Jain <jai.jain@acsicorp.com>
2023-04-06 15:04:54 +0530

    Added CustomAuth url

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from user.views import GetRequest
+from user.views import GetRequest,CustomAuthToken
 from rest_framework.authtoken.views import obtain_auth_token
 
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('crudoperations/', GetRequest.as_view({'get': 'list', 'post': 'create'}), name="list"),
     path('crudoperations/<int:pk>/',
          GetRequest.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
-    path('gettoken/', obtain_auth_token)
-
+    path('gettoken/', obtain_auth_token), # normal token
+    path('login/',CustomAuthToken.as_view()) # customized data with token
 ]
