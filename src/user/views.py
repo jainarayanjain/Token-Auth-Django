a44685ce0f2f3b51d49167b51d3c9d75b6e6d29a
Jai Jain <jai.jain@acsicorp.com>
2023-04-06 15:05:16 +0530

    Added CustomAuthToken View

--- src/user/views.py
+++ src/user/views.py
@@ -1,17 +1,24 @@
-import json
-
-from django.core.serializers import serialize
-from django.http import JsonResponse
-
 from user.models import EmployeeDetails
 from user.serializers import EmployeeSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
 class GetRequest(viewsets.ModelViewSet):
-
     queryset = EmployeeDetails.objects.all()
     serializer_class = EmployeeSerializer
 
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
